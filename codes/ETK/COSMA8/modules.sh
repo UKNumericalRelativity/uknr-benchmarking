5a344f3900ca2f679a0a2eecf7a1fb01f4228259
Miren Radia <miren_radia@yahoo.co.uk>
2026-03-27 16:21:37 +0000

    ETK: Add COSMA8 module script
    Like for Tursa, this is only used at runtime. The environment is set up
    from the config in the simfactory machine database at build time.

---
+++ codes/ETK/COSMA8/modules.sh
@@ -0,0 +1,13 @@
+module purge
+module load intel_comp/2025.3.0
+module load compiler-rt/latest
+module load tbb/latest
+module load umf/latest
+module load compiler/latest
+module load mpi/latest
+module load mkl/latest
+module load ucx/1.18.1
+module load parallel_hdf5/1.14.4
+module load gsl/2.8
+module load hwloc/2.11.1
+module load papi/7.0.1
